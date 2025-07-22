d61676b2846e5ef82188b78c7104d8239210c23d
ricountzero <donnutello@protonmail.com>
2025-07-23 01:39:01 +0300

    Rolling back previous solution. It was more efficient.

--- python/morse_kate/morse_kate.py
+++ python/morse_kate/morse_kate.py
@@ -1,13 +1,6 @@
 import machine, neopixel
 from time import sleep
 
-LEDpin = 16
-LED_PURPLE = (128, 0, 128)
-DOT_DURATION = 0.2
-DASH_DURATION = 0.6
-SPACE_DURATION = 0.2
-LETTER_SPACE_DURATION = 0.6
-
 morse_code = {
     'K': '-.-',
     'A': '.-',
@@ -15,45 +8,33 @@ morse_code = {
     'E': '.'
 }
 
+LEDpin = 16
+LED_PURPLE = (128, 0, 128)
+DOT_DURATION = 0.2
+DASH_DURATION = 0.6
+SPACE_DURATION = 0.2
+LETTER_SPACE_DURATION = 0.6
+
 led = neopixel.NeoPixel(machine.Pin(LEDpin), 1)
 
-def set_led_color(color):
-    led[0] = color
-    led.write()
-
-def sleep_duration(duration):
-    sleep(duration)
-
-def turn_on_led():
-    set_led_color(LED_PURPLE)
-
-def turn_off_led():
-    set_led_color((0, 0, 0))
-
 def blink(duration):
-    turn_on_led()
-    sleep_duration(duration)
-    turn_off_led()
-    sleep_duration(SPACE_DURATION)
+    led[0] = LED_PURPLE
+    led.write()
+    sleep(duration)
+    led[0] = (0, 0, 0)
+    led.write()
+    sleep(SPACE_DURATION)
 
-def get_morse_symbols(letter):
-    return morse_code[letter]
+def blink_morse(letter):
+    for symbol in morse_code[letter]:
+        if symbol == '.':
+            blink(DOT_DURATION)
+        elif symbol == '-':
+            blink(DASH_DURATION)
 
-def process_symbol(symbol):
-    duration = DOT_DURATION if symbol == '.' else DASH_DURATION
-    blink(duration)
-
-def process_letter(letter):
-    symbols = get_morse_symbols(letter)
-    for symbol in symbols:
-        process_symbol(symbol)
-    turn_off_led()
-    sleep_duration(LETTER_SPACE_DURATION)
-
-def main():
-    while True:
-        for letter in "KATE":
-            process_letter(letter)
-
-if __name__ == "__main__":
-    main()
+while True:
+    for letter in "KATE":
+        blink_morse(letter)
+        led[0] = (0, 0, 0)
+        led.write()
+        sleep(LETTER_SPACE_DURATION)
